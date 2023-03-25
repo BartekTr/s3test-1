86d13d67e57b4693beacececd22447a07e8bb5f5
Bartektrr <109850860+Bartektrr@users.noreply.github.com>
2023-03-25 21:15:31 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,38 @@ const AWS = require("aws-sdk");
 const s3 = new AWS.S3()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({
-    status: "success",
-    content: "text",
-  });
+router.get('/', async function(req, res, next) {
+  let my_file = await s3.getObject({
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "content.json",
+  }).promise()
+  const result = JSON.parse(my_file.Body)?.content
+  if(result == null) {
+    res.json({
+      status: "fail"
+    });  
+  }
+  else {
+    res.json({
+      status: "success",
+      content: result,
+    });  
+  }
 });
 
 router.post("/", async (req, res) => {
-  const {number} = req.body;
+  const {content} = req.body;
+  const contentObj = {
+    content: content
+  }
+  await s3.putObject({
+    Body: JSON.stringify(contentObj, null, 2),
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "content.json",
+  }).promise()
   res.json({
     status: "success",
-    newFavouriteNumber: number,
+    content: content,
   });
 });
 
